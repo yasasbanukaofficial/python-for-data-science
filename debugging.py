eb85cabcf6f60f909a5c91d8e0127400da5a390d
yasas banuka <yixbinfo@gmail.com>
2026-06-11 16:53:02 +0530

    feat: add debugging functionality and multiplication function
    - Introduced a new file `debugging.py` for debugging purposes.
    - Implemented a `multipication` function to multiply two numbers.
    - Added interactive input prompts for user to enter numbers.
    - Included a breakpoint using pdb for step-by-step debugging.

---
+++ debugging.py
@@ -0,0 +1,11 @@
+import pdb
+def multipication(a,b):
+    answer = a * b
+    return answer
+
+# pdb.set_trace()
+breakpoint()
+x = input("Enter first number:  ")
+y = input("Enter second number:  ")
+mul = multipication(x, y)
+print(mul)
